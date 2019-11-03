2de0d0901c41347f2a55bff8b5d17b2210eb37b6
li.qiang <2519941818@qq.com>
2019-11-03 11:34:58 +0800

    first commit

---
+++ c-demo/cmake-build-debug/CMakeFiles/c_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_demo.dir/main.c.obj"
+  "c_demo.pdb"
+  "c_demo.exe"
+  "c_demo.exe.manifest"
+  "libc_demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
